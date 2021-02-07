8ee6c6aa7e5f697246a6ebb052ede8b258024635
Glitch (hello-webpage) <none>
2021-02-07 14:00:42 +0000

    👕🚤 Checkpoint ./script.js:27994241/181

--- script.js
+++ script.js
@@ -5,4 +5,4 @@ console.clear()
 console.clear()
 console.log("%c Lofi Hip Hop Beats To Relax / Study To ✍🏫", " color: aqua");
 console.log("%c I hope this Website Helps You Relax / Study Better 🚀✍", " color: blue");
-
+console.log("%c Have a good day 👋✍", " color: navy");
