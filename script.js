67bbd8e09f76d606877ae61dba13e5a290216d34
Glitch (hello-webpage) <none>
2021-02-07 01:01:48 +0000

    🍪🐪 Checkpoint ./script.js:27994241/2565

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Lofi Hip Hop Beats To Relax / Study To ✍🏫");
+console.clear();
+console.clear()
+console.clear()
+console.clear()
+console.clear()
+console.log("%c Lofi Hip Hop Beats To Relax / Study To ✍🏫", " color: aqua");
+console.log("%c I hope this Website Helps You Relax / Study Better 🙂space", " color: aqua");
