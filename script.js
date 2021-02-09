76aa87ec82624395cf867487b3a03e0307f2c603
Glitch (lofi-hip-hop-beats) <none>
2021-02-09 01:19:54 +0000

    Rewound to commit a2f6aef92440d712a3df2352e7c6ab55e6875f78 + 0 edits

--- script.js
+++ script.js
@@ -10,33 +10,45 @@ console.log(
   " color: blue"
 );
 console.log("%c Have a good day 👋✍", " color: navy");
-dragElement(document.getElementById("mydiv"));
+console.log('OMG Rainbows!'.rainbow);
 
+
+dragElement(document.getElementById("mydiv"));
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
-     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
-     elmnt.onmousedown = dragMouseDown;
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    pos3 = e.clientX; pos4 = e.clientY;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
     document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    pos1 = pos3 - e.clientX; 
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
-    pos3 = e.clientX; 
+    pos3 = e.clientX;
     pos4 = e.clientY;
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
   }
 
   function closeDragElement() {
